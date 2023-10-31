2fb1395b78cb7f2ab54fbeaf7c604498ce57afc8
Ujjwal <ujjals267@gmail.com>
2023-10-31 16:23:48 +0530

    registration page form validate and responsive made

--- src/pages/DefaultHome.jsx
+++ src/pages/DefaultHome.jsx
@@ -15,6 +15,14 @@ import LoginPopup from '../Components/Login and Signup/LoginPopup';
 import HeroProductPage from '../Components/HeroProductPage/HeroProductPage';
 import DogSuppliesBanner from "../Images/DogsPage/dogsupplies.png";
 import Contact from '../Components/Support/Contact';
+import Blog from '../Components/Blog/Blog';
+import Vet from '../Components/Vet/Vet';
+import Thankyou from '../Components/Thankyou/Thankyou';
+import AboutUs from '../Components/AboutUs/AboutUs';
+import PrivacyPolicy from '../Components/PrivacyPolicy/PrivacyPolicy';
+import OrderOverview from '../Components/OrderOverview/OrderOverview';
+import OrderTracking from '../Components/OrderTracking/OrderTracking';
+import FAQ from '../Components/FAQ/FAQ';
 
 
 const DefaultHome = () => {
@@ -31,6 +39,16 @@ const DefaultHome = () => {
 
           {/* These are pages from footer */}
           {type === 'contactus' && <Contact/>}
+          {type === 'blog' && <Blog/>}
+          {type === 'vet' && <Vet/>}
+          {type === 'faq' && <FAQ/>} 
+          {type === 'thankyou' && <Thankyou/>}
+          {type === 'aboutus' && <AboutUs/>}
+          {type === 'privacypolicy' && <PrivacyPolicy/>}
+          {type === 'orderoverview' && <OrderOverview/>}
+          {type === 'ordertracking' && <OrderTracking/>}
+
+
 
 
           {pt === 'food' && <Dog/> }
